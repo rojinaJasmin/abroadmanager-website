3e42fd50e20bd255369a71ca2a474a8374f6235b
rishav-pandey <27219302+rishav-pandey@users.noreply.github.com>
2018-03-01 20:43:51 +0530

    fixed color while scroll

--- js/script.js
+++ js/script.js
@@ -3,14 +3,18 @@ $(document).ready(function() {
 	$(window).on('scroll', function() {
 		var scroll = $(window).scrollTop();
 
-		if (scroll >= 50) {
+		if (scroll >= 30) {
 			$('#header-nav').addClass('fixed');
 			$('#nav-list a').css("color", "black");
 			$('.navbar-brand h1').css("color", "black");
-		} else {
+			$('.navbar-toggle span').css("border", "1px solid black");
+		} 
+		else 
+		{
 			$('#header-nav').removeClass('fixed');
 			$('#nav-list a').css("color", "#eee");
 			$('.navbar-brand h1').css("color", "#eee");
+			$('.navbar-toggle span').css("border", "1px solid #fff");
 		}
 	});
 });
