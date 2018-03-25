f84162b3844222403d7f04ae1fbd0e3b72f88c88
rishav-pandey <27219302+rishav-pandey@users.noreply.github.com>
2018-03-26 04:16:23 +0530

    modified necessary changes

--- js/script.js
+++ js/script.js
@@ -1,17 +1,35 @@
 $(document).ready(function() {
 	// navigation onmouseover
-	$('.dropdown').on('mouseover', function () {
-		$('#nav-list > li.dropdown').addClass("open");
+	$('.dropdown1').on('mouseover', function () {
+		$('#nav-list > li.dropdown1').addClass("open");
 
 	})
 
-	$('.dropdown').on('mouseout', function () {
-		$('#nav-list > li.dropdown').removeClass("open");
+	$('.dropdown1').on('mouseout', function () {
+		$('#nav-list > li.dropdown1').removeClass("open");
 	})
+
+  $('.dropdown2').on('mouseover', function () {
+    $('#nav-list > li.dropdown2').addClass("open");
+
+  })
+
+  $('.dropdown2').on('mouseout', function () {
+    $('#nav-list > li.dropdown2').removeClass("open");
+  })
+
+  $('.dropdown3').on('mouseover', function () {
+    $('#nav-list > li.dropdown3').addClass("open");
+
+  })
+
+  $('.dropdown3').on('mouseout', function () {
+    $('#nav-list > li.dropdown3').removeClass("open");
+  })
 
 	// Login Page
 	$('.form').find('input, textarea').on('keyup blur focus', function (e) {
-  
+
   	var $this = $(this),
       label = $this.prev('label');
 
@@ -21,20 +39,20 @@ $(document).ready(function() {
         } else {
           label.addClass('active highlight');
         }
-    	} 
+    	}
     else if (e.type === 'blur') {
       if( $this.val() === '' ) {
-        label.removeClass('active highlight'); 
-      	} 
+        label.removeClass('active highlight');
+      	}
       	else {
-        	label.removeClass('highlight');   
-      	}   
-    	} 
+        	label.removeClass('highlight');
+      	}
+    	}
     	else if (e.type === 'focus') {
-      
+
       	if( $this.val() === '' ) {
-        	label.removeClass('highlight'); 
-      	} 
+        	label.removeClass('highlight');
+      	}
       else if( $this.val() !== '' ) {
         	label.addClass('highlight');
       	}
@@ -43,18 +61,18 @@ $(document).ready(function() {
   });
 
 	$('.tab a').on('click', function (e) {
-	  
+
 	  e.preventDefault();
-	  
+
 	  $(this).parent().addClass('active');
 	  $(this).parent().siblings().removeClass('active');
-	  
+
 	  target = $(this).attr('href');
 
 	  $('.tab-content > div').not(target).hide();
-	  
+
 	  $(target).fadeIn(600);
-	  
+
 	});
 
 });
